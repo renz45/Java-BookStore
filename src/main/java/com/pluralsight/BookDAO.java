872145259512858d746588d71799fd45c67d4b87
sarah <sarah-holderness@pluralsight.com>
2018-05-06 22:08:20 -0400

    Added Task 7 test.

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -104,4 +104,22 @@ public class BookDAO {
         e.printStackTrace();
       }
     }
+
+    public void updateBook(Book book) {
+        String sql = "UPDATE book SET title = ?, author = ?, price = ?" +
+                     " WHERE book_id = ?";
+
+        try {
+          PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+          statement.setString(1, book.getTitle());
+          statement.setString(2, book.getAuthor());
+          statement.setFloat(3, book.getPrice());
+          statement.setInt(4, book.getId());
+
+          statement.executeUpdate();  //boolean rowUpdated = statement.executeUpdate() > 0;
+          statement.close();
+        } catch(SQLException e) {
+          e.printStackTrace();
+        }
+    }
 }
