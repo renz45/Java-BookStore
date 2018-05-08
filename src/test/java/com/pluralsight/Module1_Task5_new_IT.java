230aca83d02b614962cf014c9524845b43193b8d
sarah <sarah-holderness@pluralsight.com>
2018-05-08 12:36:26 -0400

    Separating tasks, created Module 1 - Tasks 5 and 6

---
+++ src/test/java/com/pluralsight/Module1_Task5_new_IT.java
@@ -0,0 +1,60 @@
+package com.pluralsight;
+
+import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.lang.reflect.Method;
+
+import java.io.*;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DriverManager.class, PreparedStatement.class, BookDAO.class})
+public class Module1_Task5_new_IT {
+
+    // Verify the deleteBook() method exists in BookDAO
+    @Test
+    public void module1_task3() throws Exception {
+      Method method = null;
+      String sql = "DELETE FROM book WHERE id = ?";
+      Connection mockConnection = Mockito.mock(Connection.class);
+      PreparedStatement mockStatement = Mockito.mock(PreparedStatement.class);
+      BookDAO bookDAO = new BookDAO(mockConnection);
+      BookDAO spyBookDAO = Mockito.spy(bookDAO);
+      boolean called_prepareStatement = false;
+
+
+      Mockito.when(mockConnection.prepareStatement(sql)).thenReturn(mockStatement);
+
+      try {
+         method =  BookDAO.class.getMethod("deleteBook", int.class);
+      } catch (NoSuchMethodException e) {
+         //e.printStackTrace();
+      }
+
+      String message = "The method deleteBook() doesn't exist in BookDAO.java.";
+      assertNotNull(message, method);
+
+      try {
+        method.invoke(spyBookDAO, 0);
+      } catch (Exception e) {}
+
+      try {
+        Mockito.verify(mockConnection,Mockito.atLeast(1)).prepareStatement(sql);
+        called_prepareStatement = true;
+      } catch (Throwable e) {}
+
+      message = "The method deleteBook() doesn't call prepareStatement() correctly.";
+      assertTrue(message, called_prepareStatement);
+    }
+}
